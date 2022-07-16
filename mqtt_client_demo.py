4764150ffb011fefacc31a94b3196e135144093e
ramadanidina <ramadaniainur2@gmail.com>
2022-07-16 10:06:48 +0700

    tugas assingment-4

---
+++ mqtt_client_demo.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "broker.hivemq.com"  # for online version
+port = 1883
+timeout = 60
+
+username = 'campuspedia'
+password = 'qlue'
+
+topic = "MAN1PONOROGO-3/bagas2"
+ 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+    # Subscribing in on_connect() - if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(topic)
+ 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode('utf-8')))
+    payload_decoded = msg.payload.decode('utf-8')
+    if payload_decoded == "On":
+        print("Lampu dinyalakan")
+        # Do something
+
+
+    elif payload_decoded == "Off":
+        print("Lampu dimatikan")
+        # Do something else
+    else:
+        print("perintah tidak di kenali")
+
+
+# Create an MQTT client and attach our routines to it.
+client = mqtt.Client("fitrahfajar1")
+client.username_pw_set(username=username,password=password)
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(broker, port, timeout)
+
+client.loop_forever()
---
+++ mqtt_publish_demo.py
@@ -0,0 +1,37 @@
+
+import paho.mqtt.client as mqtt
+from time import sleep
+
+# define static variable
+# broker = "localhost" # for local connection
+broker = "broker.hivemq.com"  # for online version
+port = 1883
+timeout = 60
+
+username = 'campuspedia'
+password = 'qlue'
+topic = "MAN1PONOROGO-3/bagas"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+def on_publish(client,userdata,result):
+    print("data published \n")
+
+
+
+client1 = mqtt.Client("Fajar2")
+client1.username_pw_set(username=username,password=password)
+client1.on_connect = on_connect
+client1.on_publish = on_publish
+client1.connect(broker,port,timeout)
+
+
+count = 0
+while count < 5:
+    status = str(input("Do you want the lamp turn On or Off? "))
+    message = status
+    ret = client1.publish(topic,payload=message)
+    sleep(1)
+    count = count + 1
+
